a612d895d9920f3d0e5331d1d7a274ca3d7e4e76
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2025-01-28 17:21:37 +0000

    Manual corregido

--- Frontend/src/pages/Help.tsx
+++ Frontend/src/pages/Help.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Container, Typography, Paper, Button } from '@mui/material';
+import { Link } from 'react-router-dom'; // Importa Link de react-router-dom
 import Menu from '../components/Menu';
 
 const Ayuda: React.FC = () => {
@@ -12,17 +13,13 @@ const Ayuda: React.FC = () => {
             Manual de Usuario
           </Typography>
           <Typography variant="body1" color="text.secondary" paragraph>
-            Para acceder al manual de usuario, haz clic en el siguiente botón:
+            Para acceder al manual de usuario, haz clic en el siguiente enlace:
           </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            href="/ManualDeUsuarioDePabloVegaAlamo.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Abrir Manual de Usuario
-          </Button>
+          <Link to="/ManualDeUsuarioDePabloVegaAlamo.pdf" target="_blank" style={{ textDecoration: 'none' }}>
+            <Button variant="contained" color="primary">
+              Abrir Manual de Usuario
+            </Button>
+          </Link>
         </Paper>
       </Container>
     </Box>
